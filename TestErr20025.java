71c1a06cabd0b63e2d36c7041265b9427d647ac0
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-28 21:52:43 -0800

    Create TestErr20025.java

---
+++ TestErr20025.java
@@ -0,0 +1,21 @@
+// Nama : Sukmawati/13020210025
+public class TestErr20025 {
+    public static void main(String[] args) {
+        int tot = 0;
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            tot += i;
+            if (i < j) {
+                System.out.println(i);
+            } else {
+                System.out.println(j);
+            }
+        }
+        do {
+            j++;
+        } while (j < 10);
+        while (j < 10) {
+            j++;
+        }
+    }
+}
