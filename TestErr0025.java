acc76c2ac47f4cbd7a3e69ef57d7c2933fb28c69
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-28 21:51:08 -0800

    Create TestErr0025.java

---
+++ TestErr0025.java
@@ -0,0 +1,15 @@
+//Nama : Sukmawati/13020210025
+
+import java.util.Scanner;
+
+public class TestErr0025 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        int k = 100;
+        char c = 's';
+        boolean falses = true;
+        int j = i + 1;
+        System.out.println("j adalah " + j + ", k adalah " + k + ", dan c adalah " + c + ", dan falses adalah " + falses);
+    }
+}
